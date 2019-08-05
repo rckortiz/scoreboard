d693768eec9ca3456d2a4bfd85f81ed070624494
Rick Ortiz <rick@Ricks-MBP.localdomain>
2019-08-05 14:56:24 -0400

    WIP

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+const team1Input = document.querySelector('.team-1-input')
+const team2Input = document.querySelector('.team-2-input')
 
-document.addEventListener('DOMContentLoaded', main)
+let team1Name = document.querySelector('.team-1-name')
+let team2Name = document.querySelector('.team-2-name')
+
+let team1Score = document.querySelector('.team-1-score')
+let team2Score = document.querySelector('.team-2-score')
+
+const team1Add = document.querySelector('team-1-add-1-button')
+const team2Add = documemt.querySelector('team-2-add-1-button')
+
+const team1Subtract = document.querySelector('.team-1-subtract')
+const team2Subtract = document.querySelector('.team-2-subtract')
+
+const updateTeam1button = document.querySelector('.update-team-1-button')
+const updateTeam2button = document.querySelector('.update-team-2-button')
